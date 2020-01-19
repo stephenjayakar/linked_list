86d6e7863fec36be68554648d01b2978052d815a
Stephen Jayakar <aj12ay@gmail.com>
2020-01-19 08:56:10 +0200

    no optimization test linked_list::contains ... bench:      11,430 ns/iter (+/- 5,011)

---
+++ src/lib.rs
@@ -0,0 +1,4 @@
+#![feature(test)]
+#![feature(termination_trait_lib)]
+extern crate test;
+pub mod linked_list;
--- src/linked_list.rs
+++ src/linked_list.rs
@@ -1,12 +1,13 @@
 use std::fmt;
+use std::ops::Deref;
 pub struct LinkedList<T> {
-    head: Option<LinkedListNode<T>>,
+    head: Option<Box<LinkedListNode<T>>>,
     size: usize,
 }
 
 struct LinkedListNode<T> {
     val: T,
-    next: Box<Option<LinkedListNode<T>>>,
+    next: Option<Box<LinkedListNode<T>>>,
 }
 
 pub struct LinkedListIterator<'a, T> {
@@ -22,25 +23,46 @@ impl<T> LinkedList<T> {
     }
     pub fn iter(&self) -> LinkedListIterator<T> {
         LinkedListIterator {
-            pointer: self.head.as_ref(),
+            pointer: self.head.as_ref().map(|boxed_lln| boxed_lln.deref()),
         }
     }
-    pub fn push(&mut self, val: T) {
+    pub fn append(&mut self, val: T) {
         let new_node = LinkedListNode {
             val,
-            next: Box::new(None),
+            next: None,
         };
         self.size += 1;
         match &mut self.head {
-            None => self.head = Some(new_node),
+            None => self.head = Some(Box::new(new_node)),
             Some(node) => {
                 let mut pointer = node;
-                while let Some(ref mut p) = *pointer.next {
+                while let Some(ref mut p) = pointer.next {
                     pointer = p;
                 }
-                pointer.next = Box::new(Some(new_node));
+                pointer.next = Some(Box::new(new_node));
             }
         }
+    }
+    pub fn pop(&mut self) -> Option<T> {
+        let head = self.head.take()?;
+        self.head = head.next;
+        self.size -= 1;
+        Some(head.val)
+    }
+    pub fn is_empty(&self) -> bool {
+        self.len() != 0
+    }
+    pub fn len(&self) -> usize {
+        self.size
+    }
+    pub fn clear(&mut self) {
+        self.size = 0;
+        self.head = None;
+    }
+}
+impl<T: PartialEq> LinkedList<T> {
+    pub fn contains(&self, x: &T) -> bool {
+        self.iter().any(|e| e == x)
     }
 }
 
@@ -49,7 +71,7 @@ impl<'a, T> Iterator for LinkedListIterator<'a, T> {
     fn next(&mut self) -> Option<Self::Item> {
         if let Some(p) = self.pointer {
             let ret_val = &p.val;
-            self.pointer = (*p.next).as_ref();
+            self.pointer = p.next.as_ref().map(|boxed_lln| boxed_lln.deref());
             Some(ret_val)
         } else {
             None
@@ -73,4 +95,54 @@ impl<T> fmt::Debug for LinkedList<T>
         }
         f.write_str(")")
     }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn print_test() {
+        let mut ll = LinkedList::<i32>::new();
+        ll.append(1);
+        ll.append(2);
+        ll.append(3);
+        assert_eq!(format!("{:?}", ll), "(1 . 2 . 3)");
+    }
+
+    #[test]
+    fn sanity_pop() {
+        let mut ll = LinkedList::<i32>::new();
+        ll.append(1);
+        ll.append(2);
+        ll.append(3);
+        assert_eq!(1, ll.pop().unwrap());
+    }
+
+    #[test]
+    fn sanity_contains() {
+        let mut ll = LinkedList::<i32>::new();
+        ll.append(1);
+        ll.append(2);
+        ll.append(3);
+        assert!(ll.contains(&1));
+        assert!(ll.contains(&2));
+        assert!(ll.contains(&3));
+        assert!(!ll.contains(&4));
+    }
+}
+
+use test::Bencher;
+use std::process::Termination;
+const N: i32 = 10000;
+#[bench]
+fn contains(b: &mut Bencher) -> impl Termination {
+    let mut ll = LinkedList::<i32>::new();
+    for i in 0..N {
+        ll.append(i);
+    }
+    let guess = -1;
+    b.iter(|| {
+        ll.contains(&guess);
+    })
 }
